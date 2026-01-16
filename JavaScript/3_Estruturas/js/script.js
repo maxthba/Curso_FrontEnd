6b960efdf469a1f70dd7b98ffcd3217223ca639f
Max <maxthomaz2006@gmail.com>
2026-01-16 12:26:12 -0300

    commitando parte sobre variaveis do capitulo

---
+++ JavaScript/3_Estruturas/js/script.js
@@ -0,0 +1,18 @@
+// 1 - variaveis
+let nome = "Max";
+
+console.log(nome);
+
+nome = "Max Thomazini";
+
+console.log(nome);
+
+const idade = 19;
+
+console.log(idade);
+
+//idade = 20;
+
+console.log(typeof nome);
+console.log(typeof idade);
+
